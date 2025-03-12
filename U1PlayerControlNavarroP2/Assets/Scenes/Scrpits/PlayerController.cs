46f33689d2a32f3bea79c116722a9e8df4f6ec04
M4ND017 <732296@student.dusd.net>
2025-03-12 09:58:58 -0700

    i almost finished the 4th section
    i almost finished the 4th section.

--- U1PlayerControlNavarroP2/Assets/Scenes/Scrpits/PlayerController.cs
+++ U1PlayerControlNavarroP2/Assets/Scenes/Scrpits/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerControllerr : MonoBehaviour
 {
-    float speed = 20.0f;
+    float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +18,11 @@ public class PlayerControllerr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Verticle");
+
         //We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
