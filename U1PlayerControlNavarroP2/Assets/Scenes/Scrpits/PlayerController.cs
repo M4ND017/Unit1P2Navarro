9f680dbe2fe24ebe92a644c4c347d3b6cd5b789a
M4ND017 <732296@student.dusd.net>
2025-03-14 09:57:45 -0700

    i almost finished
    i mostly finished i just need to do the extra stuff.

--- U1PlayerControlNavarroP2/Assets/Scenes/Scrpits/PlayerController.cs
+++ U1PlayerControlNavarroP2/Assets/Scenes/Scrpits/PlayerController.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControllerr : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +19,10 @@ public class PlayerControllerr : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Verticle");
-
-        //We'll move the vehicle forward
+        forwardInput = Input.GetAxis("Vertical");
+        //We'll move the veichle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        //Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up,turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
